e4740b754f0d3de805464fcd1f9cd09232ea206f
Anand Vaidya <anand1711@gmail.com>
2020-03-17 20:47:17 +0530

    change edit call to 2 seconds

---
+++ precache-manifest.77448edb122f269afd6ca0ada5e339c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef8b3c3a4404676d43e0226f496e9c36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce0db9e7077031a336ee",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "a2a84f8362076321d774",
+    "url": "/static/css/main.d0d55cb4.chunk.css"
+  },
+  {
+    "revision": "ce0db9e7077031a336ee",
+    "url": "/static/js/2.087f5813.chunk.js"
+  },
+  {
+    "revision": "a52d65c95a2d7ffcb9ff212c4d38dff4",
+    "url": "/static/js/2.087f5813.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2a84f8362076321d774",
+    "url": "/static/js/main.fbd9df02.chunk.js"
+  },
+  {
+    "revision": "7ffe976c74292b1255f7",
+    "url": "/static/js/runtime-main.8261f036.js"
+  }
+]);
